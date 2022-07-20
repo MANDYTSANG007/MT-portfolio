1f48a2f809f7ecf62b2026f084fdfb79a698d6d1
Mandy Tsang <mandy.tsang007@gmail.com>
2022-07-20 14:46:06 -0700

    add project tools and descriptions in data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Nav from "./components/nav/Nav";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import About from "./components/about/About";
-import Portfolio2 from "./components/portfolio/Portfolio2";
+// import Portfolio2 from "./components/portfolio/Portfolio2";
+import Portfolio3 from "./components/portfolio/Portfolio3";
 import Contact from "./components/contact/Contact";
 
 import { Route, Routes } from "react-router-dom";
@@ -17,7 +18,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Header />} />
             <Route path="/About" element={<About />} />
-            <Route path="/Portfolio" element={<Portfolio2 />} />
+            <Route path="/Portfolio" element={<Portfolio3 />} />
             <Route path="/Contact" element={<Contact />} />
         </Routes>
       <Footer />
