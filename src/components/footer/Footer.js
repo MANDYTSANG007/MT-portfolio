e26d869059d1c8759e4e7f63b54f04f8797042d3
Mandy Tsang <mandy.tsang007@gmail.com>
2022-07-22 13:29:29 -0700

    add github link to the main page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="footer__main">
                 <a href="/" className="footer__logo">Mandy Tsang</a>
                 <div className="footer__socials">
-                    <a href="https://github.com"><GoMarkGithub /></a>
+                    <a href="https://github.com/MANDYTSANG007"><GoMarkGithub /></a>
                     <a href="/contact"><AiOutlineContacts /></a>
                 </div>
                 <div className="footer__copyright">
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -36,18 +36,20 @@ const Header = () => {
 
                     <div className="message">
                         <h3 className="message__title">About Me</h3>
-                        <h4> I am a full stack developer who love to build things with my computer. I am also 
+                        <h4> I am a full stack developer who loves to build things with my computer. I am also 
                             a lifelong learner, reader, toastmaster, and Lego fan. 
                             <br></br>
                             <br></br>
-                            After graduated from UCLA with a bachelor's degree, I've had the privilege of working at 
+                            After graduating from UCLA with a bachelor's degree, I've had the privilege of working in 
                             the highly-regulated financial industry. In 2020, I was invited to attend CES 2020 in Las Vegas 
-                            where I was introduced to many new innovative, thought-provoking technologies. 
+                            where I was introduced to many innovative, life-changing, ground-breaking technologies that solve 
+                            some of the biggest problems in the world. The invaluable experience has deepened my 
+                            passion in computer programming.
                             <br></br>
                             <br></br>
                             Recently, I have been furthering my education in application development at UC Berkeley.
                             My main focus these days is building accessible and user-friendly products. Check out some
-                            of my recent works at my portfolio page and feel free to reach out.
+                            of my recent works in my portfolio page and feel free to reach out.
                         </h4>
                     </div>
 
